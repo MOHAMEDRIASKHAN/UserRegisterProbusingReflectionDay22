3c81b47719bfbbe977b070572ffc17cbb4138c58
MOHAMED RIASKHAN <mohamedriaskhan@gmail.com>
2023-02-25 15:50:46 +0530

    UC1 to UC7

---
+++ PersonDetails.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserRegisterationProblemusingReflectionDay22
+{
+    public class PersonDetails
+    {
+        // [EnterPersonDetails("Riyas")]
+        public string FirstName
+        {
+            get
+            {
+                return
+                    FirstName;
+            }
+            set { FirstName = "RIYAS"; }
+        }
+
+        public string LastName
+        {
+            get
+            {
+                return LastName;
+            }
+
+            set { LastName = "KHAN"; }
+
+        }
+        public string Email
+        {
+            get
+            {
+                return Email;
+            }
+            set { Email = "mohamedriaskhan@gmail.com"; }
+        }
+
+        public int Number
+        {
+            get
+            {
+                return Number;
+            }
+            set { Number = 1234567890; }
+        }
+        public string Password
+        {
+            get
+            {
+                return Password;
+            }
+            set { Password = "Riyaskhan@9420"; }
+        }
+    }
+}
