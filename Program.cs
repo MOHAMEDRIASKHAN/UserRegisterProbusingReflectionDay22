40326c28385ea60cfabc4e3435978b36ea68c1dc
MOHAMED RIASKHAN <mohamedriaskhan@gmail.com>
2023-02-25 15:42:42 +0530

    Greeting Message

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+
+namespace UserRegisterationProblemusingReflectionDay22
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to UserRegisterationProblemUsingReflection");
+        }
+    }
+}
